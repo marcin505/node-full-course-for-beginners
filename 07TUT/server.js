c6b34f3a82eab7e51ee4b8289cd1a4f715386229
Marcin Galeziowski <marcin.galeziowski@gmail.com>
2023-04-18 10:15:57 +0200

    Chapter 7: Middleware 2

--- 07TUT/server.js
+++ 07TUT/server.js
@@ -4,15 +4,30 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const fsPromises = require('fs').promises;
-const logEvents = require(`./middleware/logEvents`);
+const cors = require('cors');
+const { logger } = require(`./middleware/logEvents`);
+const errorHandler = require(`./middleware/errorHandler`);
 const PORT = process.env.PORT || 3500;
 
 // custom middlware logger
-app.use((req, res, next) => {
-  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://medium.com', 'http://127.0.0.1:3000'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log({ origin });
+    // callback(new Error('Not allowed by CORS kurde'));
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:
@@ -67,8 +82,21 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.use is used for middleware, doesn't understnd regex
+
+// app.all/get/post is used for routing
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    // res.json({ error: '404 Not Found Kurde' });
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
